34936cce5ad63dd170db17ccd4196c6394a91c48
Rafael Arrieta Otero <rarrietaotero@gmail.com>
2017-08-12 13:00:08 -0500

    Cuarta Clase Diplomado Aplicaciones Moviles

---
+++ Modulo_3/lab4/index.js
@@ -0,0 +1,6 @@
+var a = "variable";
+var b = 5;
+var c = true;
+var d = ["a", "b", "c", "d"];
+var e = {};
+console.log(a + " " + b + " " + c + " " + d + " " + e);
---
+++ Modulo_3/lab4/index.ts
@@ -0,0 +1,7 @@
+let a:string = "variable";
+let b: number = 5;
+let c:boolean = true;
+let d:Array<string> = ["a","b","c","d"];
+let e:any= {};
+
+console.log(a + " " + b + " " + c + " " + d +" " + e);
